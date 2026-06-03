302be693074d7e4f029c3ef9cc8f967eae682ba7
david-alejo <churr.te@gmail.com>
2026-06-03 14:08:58 +0200

    Changed run script

--- slam/run_container_linux.bash
+++ slam/run_container_linux.bash
@@ -5,11 +5,11 @@ mkdir -p $HOME/percepcion_ar_shared
 docker run -it \
     --env="DISPLAY=$DISPLAY" \
     --env="QT_X11_NO_MITSHM=1" \
-    --name rssa \
+    --name percepcion_ar \
     --net=host \
     --privileged \
     --mount type=bind,source=$HOME/percepcion_ar_shared,target=/home/percepcion_ar \
-    rssa \
+    percepcion_ar \
     bash
     
 docker rm percepcion_ar
